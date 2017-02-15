1718ca7d199e58f09ae5ed8b54d5ec7f7789bd54
neavemj <matthewjneave1@gmail.com>
2017-02-15 16:10:40 +1100

    updating rab app

--- matts_js/rab_gene_table.js
+++ matts_js/rab_gene_table.js
@@ -1,6 +1,7 @@
 var tabulate = function (data,columns) {
   var table = d3.select('div#rab-table').append('table')
           .attr("class", "table display tableSection")
+          .attr("id", "rab-table-body")
         var thead = table.append('thead')
         var tbody = table.append('tbody')
 
@@ -28,7 +29,7 @@ var tabulate = function (data,columns) {
   return table;
 }
 
-d3.tsv('/matts_data/rab_gene_annots.tsv',function (data) {
+d3.tsv('/matts_data/rab_gene_annots_100.tsv',function (data) {
         var columns = ["Gene ID", "Description"]
         tabulate(data,columns)
 })
