44d91199047603fe9396675f1a1bc316485ed75d
neavemj <matthewjneave1@gmail.com>
2017-03-03 08:55:52 +1100

    beginning pathway tab

---
+++ matts_js/rab_pathway_table.js
@@ -0,0 +1,51 @@
+var pathway_tabulate = function (data,columns) {
+  var table = d3.select('div#rab_pathway_table').append('table')
+          .attr("class", "table display tableSection")
+          .attr("id", "rab_pathway_table_body")
+        var thead = table.append('thead')
+        var tbody = table.append('tbody')
+
+        thead.append('tr')
+          .selectAll('th')
+            .data(columns)
+            .enter()
+          .append('th')
+            .text(function (d) { return d })
+
+        var rows = tbody.selectAll('tr')
+            .data(data)
+            .enter()
+          .append('tr')
+
+        var cells = rows.selectAll('td')
+            .data(function(row) {
+                return columns.map(function (column) {
+                        return { column: column, value: row[column] }
+              })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+  initialize_pathway_dataTable();
+  return table;
+  
+};
+
+var initialize_pathway_dataTable = function(){
+	var d_table = $('#rab_pathway_table_body').DataTable( {			
+	"scrollY": "350px",
+	"scrollCollapse": false,
+	});
+ 
+$('#rab_pathway_table_body tbody').on( 'click', 'tr', function () {
+	var gene_name = $(this).closest('tr').children().first().text();
+    //update_pathway_plot(gene_name);
+	if ( $(this).hasClass('selected') ) {
+		    $(this).removeClass('selected');
+		}
+	else {
+		    d_table.$('tr.selected').removeClass('selected');
+		    $(this).addClass('selected');
+	}		
+	});
+};
